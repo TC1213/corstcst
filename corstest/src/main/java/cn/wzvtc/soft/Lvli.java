1dd4e4610a1d788551568e7f5362f923057d4dd9
xwz <43591698+TC1213@users.noreply.github.com>
2019-12-30 09:30:48 +0800

    Add files via upload

--- corstest/src/main/java/cn/wzvtc/soft/Lvli.java
+++ corstest/src/main/java/cn/wzvtc/soft/Lvli.java
@@ -10,14 +10,22 @@ public class Lvli {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String number;
-
     private String url;
-    public Lvli(){
 
+    public Long getId() {
+        return id;
     }
 
+    public String getNumber() {
+        return number;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+    public Lvli(){
+    }
     public Lvli(String number, String url) {
         this.number = number;
         this.url = url;
